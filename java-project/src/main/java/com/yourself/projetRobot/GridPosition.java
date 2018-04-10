d2def35061fb0ca84fff81013f484349edfc206f
richard urunuela <ierdre44@gmail.com>
2018-04-10 17:53:58 +0200

    add objet

--- java-project/src/main/java/com/yourself/projetRobot/GridPosition.java
+++ java-project/src/main/java/com/yourself/projetRobot/GridPosition.java
@@ -5,14 +5,10 @@ public class GridPosition {
     int x;
     int y;
     public GridPosition(int x, int y) {
-        this.x = x;
-        this.y = y;
+
     }
     @Override
     public boolean equals(Object other){
-        GridPosition oth =
-                (GridPosition)other;
-        return ((oth.x == this.x)&&
-                (this.y == oth.y));
+      return false;
     }
 }
